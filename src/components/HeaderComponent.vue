00164d70ebda2134157d2241ed55c3a7fafee1b9
poil4291 <poil4291@gmail.com>
2024-02-21 14:35:24 +0900

    jenkins_test

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -18,7 +18,7 @@
       </button>
       <ul class="navbar_nav">
         <li class="nav-item active">
-          <a href="/likes">좋아요다아아아아</a>
+          <a href="/likes">좋아요라고오오오오</a>
         </li>
         <li class="nav-item">
           <a href="/cart" target="_blank">장바구니</a>
